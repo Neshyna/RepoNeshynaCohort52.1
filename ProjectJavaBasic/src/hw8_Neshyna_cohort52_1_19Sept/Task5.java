61c4405fc2fd08e099592bddc8da7ae5931eb8b9
Neshyna <mariya.neshyna@gmail.com>
2024-10-03 19:54:38 +0200

    sum of input number by number cut

--- ProjectJavaBasic/src/hw8_Neshyna_cohort52_1_19Sept/Task5.java
+++ ProjectJavaBasic/src/hw8_Neshyna_cohort52_1_19Sept/Task5.java
@@ -1,4 +1,7 @@
 package hw8_Neshyna_cohort52_1_19Sept;
+
+import java.util.Scanner;
+
 /*
 Task 5 * (Опционально)
 Программа запрашивает у пользователя ввод произвольного числа.
@@ -10,6 +13,24 @@ Task 5 * (Опционально)
  */
 public class Task5 {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Insert random number");
+        int input = scanner.nextInt();
+        System.out.println("Scanned number: " + input);
+
+        int sum = 0;
+
+        while(input >= 1 ){
+
+            int x = input % 10;
+            input = input / 10;
+            sum += x;
+
+        }System.out.println(sum);
+
+
+
 
     }
 }
