504c26aa7845983d4d20c360583db86eb4029172
Neshyna <mariya.neshyna@gmail.com>
2024-10-07 00:04:07 +0200

    hw10 optional

--- ProjectJavaBasic/src/hw10_Neshyna_cohort52_1_20Sep/T4_HW10_Neshyna_C52_1.java
+++ ProjectJavaBasic/src/hw10_Neshyna_cohort52_1_20Sep/T4_HW10_Neshyna_C52_1.java
@@ -1,6 +1,8 @@
 package hw10_Neshyna_cohort52_1_20Sep;
 
-/*public class T4_HW10_Neshyna_C52_1 {
+import java.util.Arrays;
+
+public class T4_HW10_Neshyna_C52_1 {
     /*
     Task 4* со звездочкой (Опционально)
 Дано: Два отсортированных массива целых чисел. Вам нужно найти элемент,
@@ -23,23 +25,44 @@ k = 7
 
 7-й элемент (по счету) этого массива хранит значение 256.
      */
-    /* public static void main(String[] args) {
+    public static void main(String[] args) {
 
         int[] array1 = {100, 112, 256, 349, 770};
         int[] array2 = {72, 86, 113, 119, 265, 445, 892};
         int k = 7;// k number location
 
-
-
-    }
-    public static int joinArray (int[] array){
-
+        int[] arrayJoin = joinArray(array1,array2);
+        int kElement = kElement(arrayJoin,7);
 
     }
+    public static int[] joinArray (int[] array1, int[] array2){
 
-    public static int kElement (int[] array, int ч){
+        int[] arrayJoin = new int[array1.length + array2.length];
 
+        for (int i = 0; i < array1.length; i++) {
+            arrayJoin[i] = array1[i];
+        }
+
+        // Copy elements of the second array
+        for (int i = 0; i < array2.length; i++) {
+            arrayJoin[array1.length + i] = array2[i];
+        }
+
+        for (int i = 0; i < arrayJoin.length; i++){
+            System.out.println(arrayJoin[i]);//just printed array not formated
+
+        }
+        System.out.println(Arrays.toString(arrayJoin)); //formated array but not ascending order
+        Arrays.sort(arrayJoin);
+        System.out.println("sorted joined array: " + Arrays.toString(arrayJoin));//formated and ascending
+        return arrayJoin;
+    }
+
+
+    public static int kElement (int[] arrayJoin, int k){
+
+        System.out.println("k element: " + arrayJoin[k]);
+
+        return arrayJoin[k];
     }
 }
-*/
-//TODO
